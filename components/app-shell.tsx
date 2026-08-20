928850acb301ebde52a4393cb4b89ae3c1dfe505
Gast0nujAs <hcesargaston@hotmail.com>
2026-08-20 16:26:07 -0300

    prueba deploy

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -45,7 +45,7 @@ export function AppShell({
             <Package className="size-4" />
           </div>
           <span className="font-serif text-base font-semibold tracking-tight">
-            INOMAX
+            INOMAX123
           </span>
         </div>
 
